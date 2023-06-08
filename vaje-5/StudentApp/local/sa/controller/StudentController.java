5ac02bb73b4bba9851be150a14108b7b4c2ce7ac
srdjanfis <130354228+srdjanfis@users.noreply.github.com>
2023-06-08 20:05:00 +0200

    Add files via upload

---
+++ vaje-5/StudentApp/local/sa/controller/StudentController.java
@@ -0,0 +1,44 @@
+package local.sa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import local.sa.entity.Student;
+import local.sa.entity.User;
+import local.sa.service.StudentService;
+
+@RestController
+@RequestMapping("stud")
+public class StudentController {
+	
+	@Autowired
+	StudentService studentService;
+	
+	@PostMapping("/authenticate")
+	public String authUser(@RequestBody User u) {
+		return studentService.authUser(u.getUsername(),
+									   u.getPassword());
+	}
+	
+	@PostMapping("/addStudent")
+	public void addStudent(@RequestBody Student s) {
+		studentService.addStudent(s);
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getAllStudents(){
+		return studentService.getAllStudents();
+	}
+	
+	@GetMapping("/students/{idStudent}")
+	public Student getAllStudents(@PathVariable int idStudent){
+		return studentService.getAllStudents(idStudent);
+	}
+}
