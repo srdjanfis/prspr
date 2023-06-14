cbfbb8f221d6853034a91ce3855adf881fec52a5
srdjanfis <130354228+srdjanfis@users.noreply.github.com>
2023-06-14 19:50:53 +0200

    Delete vaje-7/local/ski directory

--- vaje-7/local/ski/repository/SkiResortRepository.java
+++
@@ -1,9 +0,0 @@
-package local.ski.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import local.ski.entity.SkiResort;
-
-public interface SkiResortRepository extends JpaRepository<SkiResort, Integer> {
-
-}
