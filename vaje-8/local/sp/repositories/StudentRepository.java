93ffdce4344867b7b976ccde9ef0da3a358ff2db
srdjanfis <130354228+srdjanfis@users.noreply.github.com>
2023-06-15 18:26:11 +0200

    Delete vaje-8 directory

--- vaje-8/local/sp/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package local.sp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import local.sp.entities.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>{
-
-}
